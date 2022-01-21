1e975e57795f1876c6628be82dd5917b6d833977
djordjevict <85800190+djordjevict@users.noreply.github.com>
2022-01-21 20:16:10 +0100

    complete project

--- reddit.py
+++ reddit.py
@@ -1,16 +1,15 @@
-#CLIENT_ID='o6LqV4RYbHv8rnkMazt01g'
 CLIENT_ID = '7_XBLzur5GVXFz7Y25NsuQ'
-#SECRET_TOKEN='HWcRqlhlss5bz0I1Z3VJ1t2chg2p0Q'
 SECRET_TOKEN = 'HzdUa_9xEzxKcMSb8kJD3VB0JMZg8g'
 
 import requests
 import pandas as pd
+from textblob import TextBlob
+import re
 
 class RedditAuth():
 
     def authenticate(self):
         auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_TOKEN)
-        #'grant_type': 'authorization_code'
         data = {'grant_type': 'password',
         'username': 'Macupikcu',
         'password': 'SljivovicaRakija15'}
@@ -21,31 +20,40 @@ class RedditAuth():
         headers['Authorization'] = f'bearer {TOKEN}'
         return headers
 
-class Ostalo():
+class RedditAnalyze():
 
     def __init__(self):
         self.headers = RedditAuth().authenticate()
 
-    def nekaFunkcija(self):                 # stavi /porn umesto /python, ja kazem, Reddit beskonacna stvar...
-        res = requests.get('https://oauth.reddit.com/r/python/hot', headers=self.headers, params={'limit':'10'})
+    def data_collection(self):                 
+        res = requests.get('https://oauth.reddit.com/r/college', headers=self.headers, params={'limit':'20'})
 
         df = pd.DataFrame()
 
         for post in res.json()['data']['children']:
-            #print(post['data'].keys())     # Da prikaze sve moguce kljuceve po kojima mozemo da izvlacimo podatke
+            temp = self.analyze_sentiment(post['data']['title'])
             df = df.append({
+                'Author': post['data']['author'],
                 'Title': post['data']['title'],
                 'Selftext': post['data']['selftext'],
-                'Upvote_ratio': post['data']['upvote_ratio'],
-                'Ups': post['data']['ups'],
-                #'Downs': post['data']['downs'],    # Zakomentarisao sam ga jer je nekako uvek 0, a ni ne postoji Downvote_ratio, vrv da se ne bedare ljudi i slicno.. kao sto su ukinuli dislike na YT
-                'Author': post['data']['author'],
-                'Name': post['data']['name']    # Nema potrebe da se kreira "fullname", ovo je "fullname"
+                'Sentiment': temp
             }, ignore_index=True)
 
         return df
+
+    def analyze_sentiment(self, text):
+        analysis = TextBlob(text)
+               
+        if analysis.polarity > 0:
+            return "Positive post"
+        elif analysis.polarity == 0:
+            return "Neutral post"
+        else:
+            return "Negative post"
         
+
 if __name__ == '__main__':
-    prom = Ostalo()
-    prom1 = prom.nekaFunkcija()
-    print(prom1)
+
+    reddit = RedditAnalyze()
+    result = reddit.data_collection()               
+    print(result)
